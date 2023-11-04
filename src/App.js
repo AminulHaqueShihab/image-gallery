e0f368804301fb3fbb0801de8d7d5110d4b1430e
AminulHaqueShihab <61130539+AminulHaqueShihab@users.noreply.github.com>
2023-11-05 01:43:47 +0600

    Initial  commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
-import Gallary from './components/Gallary.jsx';
+import Gallary from './components/gallary';
 const App = () => {
-	return (
-		<Flex w='70%' py='50px' mx='auto'>
-			<Gallary />
-		</Flex>
-	);
+	return <Flex w='70%' py='50px' mx='auto'>
+    <Gallary />
+  </Flex>;
 };
 
 export default App;
